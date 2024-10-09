ebe45f6f1671298bbdb759a3a1d2e84400d904f3
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-10-10 01:08:44 +0200

    Star Rating Project

---
+++ 25-React-Projects/src/components/accordian/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id : '1',
+        question: "What are accordian components?",
+        answer: "Acoo asd a s df asd f asdf a sdf as df asf"
+    },
+    {
+        id: '2',
+        question: "What are accordian components?",
+        answer: "Acoo asd a s df asd f asdf a sdf as df asf"
+    },
+    {
+        id: '3',
+        question: "What are accordian components?",
+        answer: "Acoo asd a s df asd f asdf a sdf as df asf"
+    },
+    {
+        id: '4',
+        question: "What are accordian components?",
+        answer: "Acoo asd a s df asd f asdf a sdf as df asf"
+    },
+];
+
+export default data
